3a7c42c4a3cf89bae9a26124db8147cd215736a4
Eric Wassenaar <e07@@nikhef.nl>
2000-06-29 16:11:46 +0000

    ping version 990522

--- vers.c
+++ vers.c
@@ -1,8 +1,8 @@
 #ifndef lint
-static char Version[] = "@(#)vers.c	e07@nikhef.nl (Eric Wassenaar) 980831";
+static char Version[] = "@(#)vers.c	e07@nikhef.nl (Eric Wassenaar) 990522";
 #endif
 
-char *version = "980831";
+char *version = "990522";
 
 #if defined(apollo)
 int h_errno = 0;
