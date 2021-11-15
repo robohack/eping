800e0ae6b62a1337d43241b4ef3fd45be4a4f107
Greg A. Woods <woods@planix.ca>
2021-11-14 17:33:25 -0800

    ready for 20211114 release!

--- vers.c
+++ vers.c
@@ -1,8 +1,7 @@
-#ifndef lint
-static char Version[] = "@(#)vers.c	e07@nikhef.nl (Eric Wassenaar) 990522";
-#endif
-
-char *version = "990522";
+/*
+ * n.b.:  don't forget to update before creating a release tag!
+ */
+char *version = "20211114";
 
 #if defined(apollo)
 int h_errno = 0;
